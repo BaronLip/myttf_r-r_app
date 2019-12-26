425ff7d656d226cc4cb661a44f9256d1154b983e
BaronLip <baronlip@gmail.com>
2019-12-25 18:59:08 -0600

    Imported applyMiddleware Thunk and applied to store.

--- myttf-frontend/src/store.js
+++ myttf-frontend/src/store.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import Reducer from './reducers/ReducersIndex'
 
 export function configureStore(){
    console.log("Creating store in store.js.")
    return createStore(
-      Reducer, 
+      Reducer,
+      applyMiddleware(thunk),
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
    );
 }
