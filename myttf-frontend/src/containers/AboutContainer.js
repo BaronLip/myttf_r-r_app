4ba833536c5c7509f9847bd9a6a807252d6b83c4
BaronLip <baronlip@gmail.com>
2020-01-26 12:19:24 -0600

    Added note to AboutContainer.js

--- myttf-frontend/src/containers/AboutContainer.js
+++ myttf-frontend/src/containers/AboutContainer.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import AboutPage from '../components/AboutPage';
 import { Grid, Segment } from 'semantic-ui-react';
 
 // This is not currently a stateful container but there's future possibility/intent with subscription e-mail.
 
+
+// Converted to functional container from class container.
 // export default class AboutContainer extends Component {
 const AboutContainer = () => {
 
