b803bd9720131a1519ffaca2fbcc6c2643182739
BaronLip <baronlip@gmail.com>
2020-03-18 15:09:33 -0500

    Updated player.rb model with has_secure_password and validations.

--- myttf-api/app/models/player.rb
+++ myttf-api/app/models/player.rb
@@ -1,4 +1,11 @@
 class Player < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true
+    validates :password, uniqueness: true
+    
     has_many :matches, dependent: :destroy
     has_many :games, through: :matches
 end
