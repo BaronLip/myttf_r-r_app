515c27c6347fd226b22ffb2f23d92ef9a881e2d1
BaronLip <baronlip@gmail.com>
2020-03-18 16:15:05 -0500

    Updated routes for login/sessions controller actions.

--- myttf-api/config/routes.rb
+++ myttf-api/config/routes.rb
@@ -1,6 +1,9 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   get "/api/v1/login", to: "sessions#create"
+  get '/api/v1/logged_in', to: 'sessions#is_logged_in?'
+  post '/api/v1/login', to: 'sessions#create'
+  delete '/api/v1/logout', to: 'sessions#destroy'
 
   namespace :api do
     namespace :v1 do
