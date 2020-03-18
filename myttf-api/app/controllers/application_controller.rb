eb3eebfd80e9a288fd58b5b5eabd4a6f8cfe1380
BaronLip <baronlip@gmail.com>
2020-03-18 15:17:25 -0500

    Added login methods to application_controller.rb

--- myttf-api/app/controllers/application_controller.rb
+++ myttf-api/app/controllers/application_controller.rb
@@ -3,4 +3,27 @@ class ApplicationController < ActionController::Base
     
     include ActionController::Helpers
     include ActionController::Cookies
+
+    helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
+
+    def login!
+        session[:player_id] = player.id
+    end
+    
+    def logged_in?
+        !!session[:player_id]
+    end
+
+    def current_user
+        current_user ||= Player.find(session[:player_id]) if session[:player_id]
+    end
+
+    def authorized_user?
+        player == current_user
+    end
+
+    def logout!
+        session.clear
+    end
+    
 end
