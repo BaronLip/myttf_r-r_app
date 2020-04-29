5e1a27631783e5da519c9aaa48e005ce1d60e9d0
BaronLip <baronlip@gmail.com>
2020-04-28 23:20:43 -0500

    Converted LoginForm.js to class component.

--- myttf-frontend/src/components/LoginForm.js
+++ myttf-frontend/src/components/LoginForm.js
@@ -1,54 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions/LoginFormActions';
 // import { Link } from 'react-router-dom';
 
-let player = {};
-const handleChange = (event) => {
-	let { name, value } = event.target;
-	player[name] = value;
-};
-
-const handleSubmit = (e) => {
-	e.preventDefault();
-	console.log(player);
+class LoginForm extends Component {
 	
-	login(player);
+	render() {
+		let player = {};
+		
+		const handleChange = (event) => {
+			let { name, value } = event.target;
+			player[name] = value;
+			console.log(player)
+		};
+
+		const handleSubmit = (e) => {
+			e.preventDefault();
+			console.log(player);
+			
+			let username = e.target[0].value;
+			let email = e.target[1].value;
+			let password = e.target[2].value;
 	
-	// // *** NO LONGER NEEDED SINCE handleChange() creates the player.
-	// let username = e.target[0].value;
-	// let email = e.target[1].value;
-	// let password = e.target[2].value;
+			player = {
+				username: username,
+				email: email,
+				password: password
+			};
 
-	// let player = {
-	// 	username: username,
-	// 	email: email,
-	// 	password: password
-	// };
-	// // *** END
-}
+			login(player);
+		}
 
-// Props are passed into a functional component as argument objects.
-// They can be passed in as "props". And values are extracted through props, ie: props.username.
-// Or be destructured and just use the key to represent the value.
-const LoginForm = ({ username, email, password }) => {
-	console.log({ username, email, password });
-	return (
-		<form onSubmit={handleSubmit}>
-			<label>Username:</label> <br />
-			<input name="username" type="text" value={username} onChange={handleChange} />
-			<br />
-			<label>Email:</label>
-			<br />
-			<input name="email" label="email" type="text" value={email} onChange={handleChange} />
-			<br />
-			<label>Password:</label>
-			<br />
-			<input name="password" label="password" type="text" value={password} onChange={handleChange} />
-			<br />
-			<input type="submit" value="log in" />
-		</form>
-	);
+		return (
+			<form onSubmit={handleSubmit}>
+				<label>Username:</label> <br />
+				<input name="username" type="text" onChange={handleChange} />
+				<br />
+				<label>Email:</label>
+				<br />
+				<input name="email" label="email" type="text" onChange={handleChange} />
+				<br />
+				<label>Password:</label>
+				<br />
+				<input name="password" label="password" type="text" onChange={handleChange} />
+				<br />
+				<input type="submit" value="log in" />
+			</form>
+		);
+	}
 };
 
 const mapStateToProps = ({ username, email, password }) => ({ username, email, password });
