203d96d3b9d1688f6e639791848362583f14bc7d
BaronLip <baronlip@gmail.com>
2020-05-30 16:07:39 -0500

    Added SignUpReducer.js file.

---
+++ myttf-frontend/src/reducers/SignUpReducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+	username: '',
+	email: '',
+	password: '',
+	errors: ''
+	
+}
+
+const signUpReducer = (state = initialState, action) => {
+	console.log(state, action);
+	switch (action.type) {
+		case "SIGNUP":
+			return {
+				player: action.player
+			}
+	default:
+		return state;
+	}
+}
+
+export default signUpReducer;
